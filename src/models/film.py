e4e5f467aa5d5bc8e49d3880d899dc2536f4ad16
Evgeniy Smirnov <smirnove@rocketdata.io>
2021-12-17 21:55:49 +0300

    ASYNC-3. Add base schema models.

--- src/models/film.py
+++ src/models/film.py
@@ -1,6 +1,7 @@
 import orjson
 
 from pydantic import BaseModel
+from typing import List
 
 
 def orjson_dumps(v, *, default):
@@ -8,11 +9,43 @@ def orjson_dumps(v, *, default):
     return orjson.dumps(v, default=default).decode()
 
 
+class InstanceSchema(BaseModel):
+    id: str
+    name: str
+
+
 class Film(BaseModel):
     id: str
+    imdb_rating: float
+    genre: List[str]
     title: str
+    description: str
+    director: str
+    actors: List[InstanceSchema]
+    writers: List[InstanceSchema]
+    actors_names: List[str]
+    writers_names: List[str]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class Genre(BaseModel):
+    id: str
+    name: str
     description: str
 
     class Config:
         json_loads = orjson.loads
-        json_dumps = orjson_dumps 
+        json_dumps = orjson_dumps
+
+
+class Person(BaseModel):
+    id: str
+    full_name: str
+    role: str
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
