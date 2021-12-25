bbf4a84f5af0cc94cee235f53b88c8dff1cf43e7
JJonny <qweer.baranovsky@gmail.com>
2021-12-25 12:51:57 +0300

    Use dependency in router endpoints

--- src/api/v1/person.py
+++ src/api/v1/person.py
@@ -1,29 +1,35 @@
 from typing import List, Optional
 
-from db.elastic import get_elastic
-from db.redis import get_redis
-
 from fastapi import APIRouter
+from fastapi.param_functions import Depends
+
+from db.elastic import get_elastic, AsyncElasticsearch
+from db.redis import get_redis, Redis
 
 from models.schemas import FilmShort, Person
 from services.person_service import get_person_service
+
 
 router = APIRouter()
 
 
 @router.get('/{person_id}', response_model=Person)
-async def person_details(person_id: str) -> Person:
-    redis = await get_redis()
-    elastic = await get_elastic()
+async def person_details(
+    person_id: str,
+    redis: Redis = Depends(get_redis),
+    elastic: AsyncElasticsearch = Depends(get_elastic)
+) -> Person:
     person_service = get_person_service(redis, elastic)
     person_info = await person_service.get_by_id(person_id)
     return person_info
 
 
 @router.get('/{person_id}/film', response_model=List[FilmShort])
-async def films_by_person(person_id: str) -> List[FilmShort]:
-    redis = await get_redis()
-    elastic = await get_elastic()
+async def films_by_person(
+    person_id: str,
+    redis: Redis = Depends(get_redis),
+    elastic: AsyncElasticsearch = Depends(get_elastic)
+) -> List[FilmShort]:
     person_service = get_person_service(redis, elastic)
     person_films_list = await person_service.get_films_by_person(person_id)
     return person_films_list
@@ -34,9 +40,9 @@ async def persons(
         limit: Optional[str] = None,
         page: Optional[str] = None,
         query: Optional[str] = None,
+        redis: Redis = Depends(get_redis),
+        elastic: AsyncElasticsearch = Depends(get_elastic),
 ):
-    redis = await get_redis()
-    elastic = await get_elastic()
     person_service = get_person_service(redis, elastic)
     persons = await person_service.search_persons(limit, page, query)
     return persons
